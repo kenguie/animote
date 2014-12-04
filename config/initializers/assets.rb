ae50017d5cae982bdc3c6b0bab20013e6b68a767
nyhunter77 <nyhunter77@yahoo.com>
2014-12-04 16:27:41 -0500

    modified assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( jquery.slides.min.js )
+Rails.application.config.assets.precompile += %w( jquery.slides.js )
 Rails.application.config.assets.precompile += %w( slideshow.css.erb )
 
 # Precompile additional assets.
